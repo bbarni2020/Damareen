b08d4ced674ba9816cf4efed1cf8be4702a22efd
Balogh Barnabás <balogh.barnabas2008@gmail.com>
2025-11-08 15:33:58 +0100

    /api deleted

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -8,15 +8,20 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
     
     CORS(app, resources={
-        r"/api/*": {
-            "origins": ["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:7621", "http://127.0.0.1:7621"],
+        r"/*": {
+            "origins": [
+                "http://localhost:3000",
+                "http://127.0.0.1:3000",
+                "http://localhost:7621",
+                "http://127.0.0.1:7621",
+            ],
             "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
             "allow_headers": ["Content-Type", "Authorization"],
-            "supports_credentials": True
+            "supports_credentials": True,
         }
     })
     
     from app.routes import api
-    app.register_blueprint(api, url_prefix='/api')
+    app.register_blueprint(api)
     
     return app
