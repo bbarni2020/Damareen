ed8e17b42d497a4ee0bd7bfde40b7c9e1feb8548
Balogh Barnabás <balogh.barnabas2008@gmail.com>
2025-11-08 18:15:23 +0100

    Deafault email update

--- backend/app/email_config.py
+++ backend/app/email_config.py
@@ -5,9 +5,9 @@ class EmailConfig:
     SMTP_SERVER = 'smtp.dreamhost.com'
     SMTP_PORT = 587
     SMTP_USE_TLS = True
-    SMTP_USERNAME = os.environ.get('EMAIL_USERNAME', 'damareen@deakteri.fun')
+    SMTP_USERNAME = os.environ.get('EMAIL_USERNAME', 'damareen@deakteri.cloud')
     SMTP_PASSWORD = os.environ.get('EMAIL_PASSWORD', 'password')
-    SENDER_EMAIL = 'damareen@deakteri.fun'
+    SENDER_EMAIL = 'damareen@deakteri.cloud'
     SENDER_NAME = 'Damareen'
     
     VERIFICATION_TOKEN_EXPIRATION_HOURS = 24
