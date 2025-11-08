15ed80b78e61cb9ccc37fed96e83cd64b3d78e50
Balogh Barnabás <balogh.barnabas2008@gmail.com>
2025-11-08 15:29:56 +0100

    FIx of aoutoupdate

--- backend/setup.sh
+++ backend/setup.sh
@@ -2,7 +2,7 @@ PORT=5000
 export PORT
 
 kill -9 $(lsof -ti ":$PORT") 2>/dev/null || true
-git pull
+git restore backend/setup.sh && git pull
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
