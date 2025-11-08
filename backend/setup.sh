a7fc9e496770fdfb8b13b3539294feb63a3339ea
Balogh Barnabás <balogh.barnabas2008@gmail.com>
2025-11-08 18:35:34 +0100

    Github error fix

--- backend/setup.sh
+++ backend/setup.sh
@@ -2,6 +2,7 @@ PORT=5000
 export PORT
 
 kill -9 $(lsof -ti ":$PORT") 2>/dev/null || true
+git fetch origin && git reset --hard origin/$(git branch --show-current)
 git pull
 python3 -m venv .venv
 source .venv/bin/activate
