a3ab7f918add3a90021d21a58c0b0cba9a204f16
Balogh Barnabás <balogh.barnabas2008@gmail.com>
2025-11-08 14:39:20 +0100

    Setup updated

--- backend/setup.sh
+++ backend/setup.sh
@@ -6,6 +6,8 @@ git pull
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
+clear
+echo "Starting backend on port $PORT..."
 export FLASK_APP=run.py
-
+echo "Running server
 python3 run.py
