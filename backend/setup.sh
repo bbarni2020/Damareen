50053f27c8b8fd71619df68d3be460b571521ea1
Balogh Barnabás <balogh.barnabas2008@gmail.com>
2025-11-08 15:30:24 +0100

    Friendlier updater

--- backend/setup.sh
+++ backend/setup.sh
@@ -2,7 +2,7 @@ PORT=5000
 export PORT
 
 kill -9 $(lsof -ti ":$PORT") 2>/dev/null || true
-git restore backend/setup.sh && git pull
+git pull
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
